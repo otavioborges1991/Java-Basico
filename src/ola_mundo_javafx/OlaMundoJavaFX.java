d921646c59ee775565cef0a79adaec1ca00f717a
Otávio Vinicios Borges <otavio.vinicios.borges@gmail.com>
2026-01-26 17:20:38 -0300

    mudanças

--- src/ola_mundo_javafx/OlaMundoJavaFX.java
+++ src/ola_mundo_javafx/OlaMundoJavaFX.java
@@ -1,5 +1,13 @@
 package ola_mundo_javafx;
 
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.*;
+
 public class OlaMundoJavaFX {
+
+    public static void main(String[] args) {
+        Application.launch(App.class, args);
+    }
     
 }
